fece3fe96640b4839933955b5961cf683ab8fdc2
Marcelo Junior <marceloadsj1@gmail.com>
2019-08-30 00:13:47 +0200

    Creating the index page to the docs, ok for now

--- docs/src/routes/_layout.svelte
+++ docs/src/routes/_layout.svelte
@@ -1,22 +1,5 @@
-<script>
-	import Nav from '../components/Nav.svelte';
+<svelte:head>
+  <title>Is-velte - Html tag as string on Svelte</title>
+</svelte:head>
 
-	export let segment;
-</script>
-
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
-
-<Nav {segment}/>
-
-<main>
-	<slot></slot>
-</main>
+<slot></slot>
--- docs/src/routes/index.svelte
+++ docs/src/routes/index.svelte
@@ -1,46 +1,78 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<script>
+  import { onMount } from "svelte";
+  import Nav from "../components/Nav.svelte";
+  import Hero from "../components/Hero.svelte";
+  import Card from "../components/Card.svelte";
+  import Code from "../components/Code.svelte";
+</script>
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+<div class="min-h-screen bg-gray-100 pb-10">
+  <div class="flex flex-col mb-5">
+    <Nav></Nav>
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+    <Hero></Hero>
+  </div>
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  <div class="flex px-5 justify-center">
+    <Card>
+      <h3 class="text-lg mb-5 text-gray-700">Motivations:</h3>
 
-	p {
-		margin: 1em auto;
-	}
+      <div>
+        <p class="mb-5 text-justify">
+          Basically, I created that component to have a bit more of flexibility
+          when creating specific Svelte components that handle multiple html
+          tags.
+        </p>
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
+        <p class="mb-5 text-justify">
+          So, the only way I manage to do this is to actually change by hand
+          some parts of a Svelte generated component code, both the Client and
+          Server version.
+        </p>
 
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
+        <p class="text-justify">
+          Because that, the peer dependency will be strict tied to a Svelte
+          release version, updating every time we had a new Svelte release.
+        </p>
+      </div>
+    </Card>
 
-<h1>Great success!</h1>
+    <Card>
+      <h3 class="text-lg mb-5 text-gray-700">How to Use:</h3>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
+      <div>
+        <Code block>yarn add is-velte</Code>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+        <p class="my-3">Then:</p>
+
+        <Code block>import Is from 'is-velte';</Code>
+
+        <p class="my-3">And:</p>
+
+        <pre class="bg-gray-700 text-gray-300 px-2 py-1 rounded">{
+`<Is tag='section'>
+  My Section
+</Is>`}</pre>
+
+        <p class="mt-5">The "tag" property is a "div" by default!</p>
+      </div>
+    </Card>
+
+    <Card>
+      <h3 class="text-lg mb-5 text-gray-700">Know Limitations:</h3>
+
+      <div class="pl-5">
+        <ul class="list-disc">
+          <li class="mb-3">
+            Current, we don't have a way to pass down events, like on:*. So I
+            will add the most common events to the Is component.
+          </li>
+
+          <li class="mb-3">
+            ...
+          </li>
+        </ul>
+      </div>
+    </Card>
+  </div>
+</div>
