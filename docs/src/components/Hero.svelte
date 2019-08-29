fb5a294b8c814d58bf461054c4b7cb677518a223
Marcelo Junior <marceloadsj1@gmail.com>
2019-08-30 00:50:50 +0200

    Fix docs .js .css paths

--- docs/src/components/Hero.svelte
+++ docs/src/components/Hero.svelte
@@ -1,39 +1,22 @@
 <script>
   import { onMount } from "svelte";
-  import { fly } from "svelte/transition";
 
   import Icon from "./Icon.svelte";
   import Code from "./Code.svelte";
-
-  let show = false;
-
-  onMount(() => {
-    show = true;
-  });
 </script>
 
 <div class="flex flex-col items-center justify-center text-center py-20 mt-5">
   <div class="mb-10">
     <div class="h-16">
-      {#if show}
-      <h1
-        class="text-5xl mb-5 leading-none text-indigo-800"
-        transition:fly="{{ delay: 100, duration: 500, y: -10, }}"
-      >
+      <h1 class="text-5xl mb-5 leading-none text-indigo-800">
         <Icon type="tag"></Icon> is-velte
       </h1>
-      {/if}
     </div>
 
     <div class="h-10">
-      {#if show}
-      <p
-        class="text-gray-500"
-        transition:fly="{{ delay: 250, duration: 500, y: -10, }}"
-      >
+      <p class="text-gray-500">
         Create any html tag in Svelte using it's name as string
       </p>
-      {/if}
     </div>
   </div>
 
